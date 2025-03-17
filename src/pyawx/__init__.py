d718c06574a13a837dfd0b5690a721a471b6fc8b
srlasagne <art-breach-capitol@duck.com>
2025-03-17 08:48:38 +0100

    feat: simplify import statements

---
+++ src/pyawx/__init__.py
@@ -0,0 +1,3 @@
+from pyawx.client import Client as Client
+from pyawx.resources import JobTemplateResource as JobTemplateResource
+from pyawx.resources import WorkflowJobTemplateResource as WorkflowJobTemplateResource
